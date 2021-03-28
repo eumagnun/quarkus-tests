8165ba0af468a51bc1cd1c93343f8aab74dae5b1
Your Name <you@example.com>
2021-03-28 23:53:22 +0000

    update

--- src/main/java/br/com/danielamaral/GreetingResource.java
+++ src/main/java/br/com/danielamaral/GreetingResource.java
@@ -1,14 +1,28 @@
 package br.com.danielamaral;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
-@Path("/hello")
+
+@Path("/api")
 public class GreetingResource {
 
+   @ConfigProperty(name = "greeting.text") 
+   String text;
+
     @GET
+    @Path("/test2")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello2() {
+        return text;
+    }
+
+    @GET
+    @Path("/test1")
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello RESTEasy Daniel";
